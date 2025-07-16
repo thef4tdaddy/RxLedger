3a0eaf15393939b6a6ea52474b4195d383e1d05f
thef4tdaddy <github@ed-david.com>
2025-07-16 12:35:30 -0500

    fix: componentSSSS not component

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase';
-import ProfileSection from '../component/account/ProfileSection';
-import NotificationSettings from '../component/account//NotificationSettings';
-import CommunitySettings from '../component/account/CommunitySettings';
-import IntegrationSettings from '../component/account/IntegrationSettings';
-import DangerZone from '../component/account/DangerZone';
+import ProfileSection from '../components/account/ProfileSection';
+import NotificationSettings from '../components/account//NotificationSettings';
+import CommunitySettings from '../components/account/CommunitySettings';
+import IntegrationSettings from '../components/account/IntegrationSettings';
+import DangerZone from '../components/account/DangerZone';
 import useAuth from '../hooks/useAuth';
 
 export default function SettingsPage() {
